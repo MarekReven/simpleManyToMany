b39caf18507b7722a3dcba58f444110385616485
MarekReven <mrk.lapinski@gmail.com>
2018-01-27 16:55:40 +0100

    Added CR

--- src/AppBundle/Controller/SimpleSql.php
+++ src/AppBundle/Controller/SimpleSql.php
@@ -8,16 +8,55 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\User;
 
-class SimpleSql
+class SimpleSql extends Controller
 {
     /**
      * @Route("/simplesql")
      */
     public function showAction(){
-        return new Response("juhuuu");
+
+        $user = new User();
+        $user->setName('Dawid');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return new Response("user added");
     }
+
+    /**
+     * @Route("simplesql/addUser/{name}")
+     */
+    public function addUser($name){
+
+        $user = new User();
+        $user->setName($name);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return new Response("user {$name} added");
+    }
+
+    /**
+     * @Route("simplesql/getAllUsers")
+     */
+    public function getAllUsers(){
+
+        $users = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+
+
+        return $this->render('simplesql/allUsers.html.twig', array('users' => $users));
+    }
+
 
 }
---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sex", type="string", length=255)
+     */
+    private $sex;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set sex
+     *
+     * @param string $sex
+     *
+     * @return Category
+     */
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    /**
+     * Get sex
+     *
+     * @return string
+     */
+    public function getSex()
+    {
+        return $this->sex;
+    }
+}
+
